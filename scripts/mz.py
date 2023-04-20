6f4839966dfd76e1e4839dd2a6fa3c7f8174b5f2
Willi Ballenthin <willi.ballenthin@gmail.com>
2023-04-20 12:06:59 +0200

    mz: implement structure view

--- scripts/mz.py
+++ scripts/mz.py
@@ -1,6 +1,7 @@
 """
-    "textual==0.19.1",
+    "textual==0.20.1",
     "rich==13.3.3",
+    "dissect.cstruct==3.6",
 """
 import os
 import sys
@@ -10,12 +11,14 @@ import logging
 import pathlib
 import argparse
 import textwrap
-from typing import List
+from typing import Any, Dict, List, Callable, Tuple
 from dataclasses import dataclass
 
 import pefile
+from dissect import cstruct
 from textual import events
 from rich.text import Text
+import rich.table
 from textual.app import App, ComposeResult
 from rich.segment import Segment
 from textual.strip import Strip
@@ -23,7 +26,9 @@ from textual.widget import Widget
 from textual.logging import TextualHandler
 from textual.widgets import Label, Header, Static
 from textual.geometry import Size
+from textual.containers import Horizontal, Container
 from textual.scroll_view import ScrollView
+from dissect.cstruct.types.enum import EnumInstance
 
 logger = logging.getLogger("mz")
 
@@ -33,6 +38,8 @@ class Context:
     path: pathlib.Path
     buf: bytearray
     pe: pefile.PE
+    cparser: cstruct.cstruct
+    renderers: Dict[str, Callable[[Any], str]]
 
 
 def w(s: str) -> Text:
@@ -169,7 +176,7 @@ class HexView(ScrollView):
         padding_end_length = self.row_length - row_data_length - padding_start_length
 
         # the bytes of data to show on this line.
-        row_buf = self.ctx.buf[row_data_offset : row_data_offset + row_data_length]
+        row_buf = self.ctx.buf[self.address + row_data_offset : self.address + row_data_offset + row_data_length]
 
         segments: List[Segment] = []
 
@@ -187,7 +194,7 @@ class HexView(ScrollView):
         #     0x00000010:
         #
         # TODO: make this 8 bytes for x64
-        segments.append(Segment(f"{row_offset:08x}:", style_address))
+        segments.append(Segment(f"{self.address + row_offset:08x}:", style_address))
         segments.append(Segment("  ", style_padding))
 
         # render hex column.
@@ -319,7 +326,209 @@ class HexTestView(Widget):
         yield HexView(self.ctx, 0x0, len(self.ctx.buf), classes="tall")
 
 
-# TODO: StructureView
+STRUCTURES = """
+    struct IMAGE_DOS_HEADER {               // DOS .EXE header
+        WORD   e_magic;                     // Magic number
+        WORD   e_cblp;                      // Bytes on last page of file
+        WORD   e_cp;                        // Pages in file
+        WORD   e_crlc;                      // Relocations
+        WORD   e_cparhdr;                   // Size of header in paragraphs
+        WORD   e_minalloc;                  // Minimum extra paragraphs needed
+        WORD   e_maxalloc;                  // Maximum extra paragraphs needed
+        WORD   e_ss;                        // Initial (relative) SS value
+        WORD   e_sp;                        // Initial SP value
+        WORD   e_csum;                      // Checksum
+        WORD   e_ip;                        // Initial IP value
+        WORD   e_cs;                        // Initial (relative) CS value
+        WORD   e_lfarlc;                    // File address of relocation table
+        WORD   e_ovno;                      // Overlay number
+        WORD   e_res[4];                    // Reserved words
+        WORD   e_oemid;                     // OEM identifier (for e_oeminfo)
+        WORD   e_oeminfo;                   // OEM information; e_oemid specific
+        WORD   e_res2[10];                  // Reserved words
+        LONG   e_lfanew;                    // File address of new exe header
+    };
+
+    enum IMAGE_FILE_MACHINE : uint16 {
+        IMAGE_FILE_MACHINE_UNKNOWN = 0x0,
+        IMAGE_FILE_MACHINE_I386 = 0x14c,
+        IMAGE_FILE_MACHINE_IA64 = 0x200,
+        IMAGE_FILE_MACHINE_AMD64 = 0x8664,
+    };
+
+    struct IMAGE_FILE_HEADER {
+        IMAGE_FILE_MACHINE Machine;
+        WORD  NumberOfSections;
+        DWORD TimeDateStamp;
+        DWORD PointerToSymbolTable;
+        DWORD NumberOfSymbols;
+        WORD  SizeOfOptionalHeader;
+        WORD  Characteristics;
+    };
+"""
+
+
+class Line(Horizontal):
+    """A line of text. Children should be Static widgets."""
+
+    DEFAULT_CSS = """
+        Line {
+            /* ensure each line doesn't overflow to subsequent row */
+            height: 1;
+        }
+
+        Line > Static {
+            /* by default, the widget will expand to fill the available space */
+            width: auto;
+        }
+    """
+
+
+class StructureView(Widget):
+    COMPONENT_CLASSES = {
+        "structureview--field-name",
+        "structureview--field-type",
+        "structureview--field-offset",
+        "structureview--field-value",
+        "structureview--field-decoration",
+    }
+
+    DEFAULT_CSS = """
+        StructureView > Static.fields {
+          margin-left: 1;
+        }
+
+        StructureView .structureview--struct-name {
+          color: $secondary;
+        }
+
+        StructureView .structureview--field-name {
+          color: $accent;
+        }
+
+        StructureView .structureview--field-type {
+          color: $text-muted;
+        }
+
+        StructureView .structureview--field-offset {
+          color: $text-muted;
+        }
+
+        StructureView .structureview--field-value {
+          color: $text;
+        }
+
+        StructureView .structureview--field-decoration {
+          color: $text-muted;
+        }
+    """
+
+    def __init__(self, ctx: Context, address: int, typename: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_class("pe-pane")
+        self.styles.height = "auto"
+
+        self.ctx = ctx
+        self.address = address
+        self.type = self.ctx.cparser.typedefs[typename]
+
+        buf = self.ctx.buf[self.address:self.address + self.type.size]
+        self.structure = self.type(buf)
+
+    def compose(self) -> ComposeResult:
+        yield Line(
+            # like: struct IMAGE_DOS_HEADER {
+            Static("struct ", classes="structureview--field-decoration"),
+            Static(self.type.name, classes="structureview--struct-name"),
+            Static(" {", classes="structureview--field-decoration")
+        )
+
+        # use a table for formatting the structure fields,
+        # so that alignment is easy.
+        table = rich.table.Table(box=None, show_header=False)
+
+        style_name = self.get_component_rich_style("structureview--field-name")
+        style_offset = self.get_component_rich_style("structureview--field-offset")
+        style_value = self.get_component_rich_style("structureview--field-value")
+        style_decoration = self.get_component_rich_style("structureview--field-decoration")
+
+        table.add_column("name", style=style_name)
+        table.add_column("=", style=style_decoration)
+        table.add_column("value", style=style_value)
+        table.add_column("@", style=style_decoration)
+        table.add_column("offset", style=style_offset)
+
+        for field in self.type.fields:
+            value = getattr(self.structure, field.name)
+
+            key = f"{self.type.name}.{field.name}"
+            if key in self.ctx.renderers:
+                value = self.ctx.renderers[key](value)
+            elif isinstance(value, int):
+                value = hex(value)
+            elif isinstance(value, EnumInstance):
+                # strip of enum name, leaving just the value.
+                # like IMAGE_FILE_MACHINE_I386
+                value = value.name
+
+            self.log(field.name)
+            self.log(type(value))
+
+            table.add_row(
+                str(field.name),
+                "=",
+                str(value),
+                "@",
+                hex(field.offset),
+            )
+
+        yield Static(table, classes="fields")
+
+        yield Line(
+            Static("}", classes="structureview--field-decoration")
+        )
+ 
+
+def render_timestamp(v: int) -> str:
+    import datetime
+
+    try:
+        return datetime.datetime.fromtimestamp(v).isoformat("T") + "Z"
+    except ValueError:
+        return "(invalid)"
+
+
+def render_bitflags(bits: List[Tuple[str, int]], v: int) -> str:
+    ret = []
+
+    for flag, bit in bits:
+        if (v & bit) == bit:
+            ret.append(flag)
+
+    return " |\n".join(ret)
+
+
+def render_characteristics(v: int) -> str:
+    bits = [
+        ("IMAGE_FILE_RELOCS_STRIPPED", 0x0001),
+        ("IMAGE_FILE_EXECUTABLE_IMAGE", 0x0002),
+        ("IMAGE_FILE_LINE_NUMS_STRIPPED", 0x0004),
+        ("IMAGE_FILE_LOCAL_SYMS_STRIPPED", 0x0008),
+        ("IMAGE_FILE_AGGRESIVE_WS_TRIM", 0x0010),
+        ("IMAGE_FILE_LARGE_ADDRESS_AWARE", 0x0020),
+        ("IMAGE_FILE_16BIT_MACHINE", 0x0040),
+        ("IMAGE_FILE_BYTES_REVERSED_LO", 0x0080),
+        ("IMAGE_FILE_32BIT_MACHINE", 0x0100),
+        ("IMAGE_FILE_DEBUG_STRIPPED", 0x0200),
+        ("IMAGE_FILE_REMOVABLE_RUN_FROM_SWAP", 0x0400),
+        ("IMAGE_FILE_NET_RUN_FROM_SWAP", 0x0800),
+        ("IMAGE_FILE_SYSTEM", 0x1000),
+        ("IMAGE_FILE_DLL", 0x2000),
+        ("IMAGE_FILE_UP_SYSTEM_ONLY", 0x4000),
+        ("IMAGE_FILE_BYTES_REVERSED_HI", 0x8000),
+    ]
+
+    return render_bitflags(bits, v)
 
 
 class PEApp(App):
@@ -356,13 +565,25 @@ class PEApp(App):
         # we can introduce background parsing later.
         pe = pefile.PE(data=buf, fast_load=False)
 
-        self.ctx = Context(path, buf, pe)
+        cparser = cstruct.cstruct()
+        cparser.load(STRUCTURES)
+
+
+        renderers = {
+            "IMAGE_FILE_HEADER.TimeDateStamp": render_timestamp,
+            "IMAGE_FILE_HEADER.Characteristics": render_characteristics,
+        }
+
+        self.ctx = Context(path, buf, pe, cparser, renderers)
 
         self.title = f"pe: {self.ctx.path.name}"
 
     def compose(self) -> ComposeResult:
         yield Header()
         yield MetadataView(self.ctx)
+
+        yield StructureView(self.ctx, self.ctx.pe.DOS_HEADER.get_file_offset(), "IMAGE_DOS_HEADER")
+        yield StructureView(self.ctx, self.ctx.pe.FILE_HEADER.get_file_offset(), "IMAGE_FILE_HEADER")
 
         yield HexTestView(self.ctx)
 
