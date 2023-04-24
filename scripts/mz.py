29984a05a5b1a888d768d5bd7f6f0d2591acab11
Willi Ballenthin <willi.ballenthin@gmail.com>
2023-04-24 20:57:49 +0200

    mz: section characteristics

--- scripts/mz.py
+++ scripts/mz.py
@@ -1044,6 +1044,50 @@ class SectionView(Static):
         entropy = self.section.entropy_H(self.section.get_data().rstrip(b"\x00"))
         table.add_row("entropy:", "%.02f" % entropy)
 
+        bits = [
+            ("IMAGE_SCN_RESERVED_1", 0x00000001),
+            ("IMAGE_SCN_RESERVED_2", 0x00000002),
+            ("IMAGE_SCN_RESERVED_4", 0x00000004),
+            ("IMAGE_SCN_TYPE_NO_PAD", 0x00000008),
+            ("IMAGE_SCN_RESERVED_10", 0x00000010),
+            ("IMAGE_SCN_CNT_CODE", 0x00000020),
+            ("IMAGE_SCN_CNT_INITIALIZED_DATA", 0x00000040),
+            ("IMAGE_SCN_CNT_UNINITIALIZED_DATA", 0x00000080),
+            ("IMAGE_SCN_LNK_OTHER", 0x00000100),
+            ("IMAGE_SCN_LNK_INFO", 0x00000200),
+            ("IMAGE_SCN_RESERVED_400", 0x00000400),
+            ("IMAGE_SCN_LNK_REMOVE", 0x00000800),
+            ("IMAGE_SCN_LNK_COMDAT", 0x00001000),
+            ("IMAGE_SCN_GPREL", 0x00008000),
+            ("IMAGE_SCN_MEM_PURGEABLE", 0x00020000),
+            ("IMAGE_SCN_MEM_16BIT", 0x00020000),
+            ("IMAGE_SCN_MEM_LOCKED", 0x00040000),
+            ("IMAGE_SCN_MEM_PRELOAD", 0x00080000),
+            ("IMAGE_SCN_ALIGN_1BYTES", 0x00100000),
+            ("IMAGE_SCN_ALIGN_2BYTES", 0x00200000),
+            ("IMAGE_SCN_ALIGN_4BYTES", 0x00300000),
+            ("IMAGE_SCN_ALIGN_8BYTES", 0x00400000),
+            ("IMAGE_SCN_ALIGN_16BYTES", 0x00500000),
+            ("IMAGE_SCN_ALIGN_32BYTES", 0x00600000),
+            ("IMAGE_SCN_ALIGN_64BYTES", 0x00700000),
+            ("IMAGE_SCN_ALIGN_128BYTES", 0x00800000),
+            ("IMAGE_SCN_ALIGN_256BYTES", 0x00900000),
+            ("IMAGE_SCN_ALIGN_512BYTES", 0x00A00000),
+            ("IMAGE_SCN_ALIGN_1024BYTES", 0x00B00000),
+            ("IMAGE_SCN_ALIGN_2048BYTES", 0x00C00000),
+            ("IMAGE_SCN_ALIGN_4096BYTES", 0x00D00000),
+            ("IMAGE_SCN_ALIGN_8192BYTES", 0x00E00000),
+            ("IMAGE_SCN_LNK_NRELOC_OVFL", 0x01000000),
+            ("IMAGE_SCN_MEM_DISCARDABLE", 0x02000000),
+            ("IMAGE_SCN_MEM_NOT_CACHED", 0x04000000),
+            ("IMAGE_SCN_MEM_NOT_PAGED", 0x08000000),
+            ("IMAGE_SCN_MEM_SHARED", 0x10000000),
+            ("IMAGE_SCN_MEM_EXECUTE", 0x20000000),
+            ("IMAGE_SCN_MEM_READ", 0x40000000),
+            ("IMAGE_SCN_MEM_WRITE", 0x80000000),
+        ]
+        table.add_row("characteristics:", render_bitflags(bits, self.section.Characteristics))
+
         yield Static(table, classes="sectionview--table")
 
         for child in self.section_children:
@@ -1222,14 +1266,38 @@ class ExportsView(Static):
 
     def compose(self) -> ComposeResult:
         yield Line(Static("export directory table:", classes="exportsview--title"))
-        yield Line(Static("  symbols:", classes="exportsview--heading"))
 
-        for entry in self.ctx.pe.DIRECTORY_ENTRY_EXPORT.symbols:
-            symbol_name = entry.name.decode("ascii")
+        if not hasattr(self.ctx.pe, "DIRECTORY_ENTRY_EXPORT"):
             yield Line(
-                Static("    "),
-                Static(symbol_name, classes="exportsview--symbol"),
+                Static("  (empty)"),
             )
+
+        else:
+
+            try:
+                dll_name = self.ctx.pe.DIRECTORY_ENTRY_EXPORT.name.partition(b"\x00")[0].decode("ascii")
+            except UnicodeDecodeError:
+                dll_name = "(invalid)"
+
+            yield Line(
+                Static("  name:      ", classes="exportsview--heading"),
+                Static(dll_name),
+            )
+
+            ts = self.ctx.pe.DIRECTORY_ENTRY_EXPORT.struct.TimeDateStamp
+            yield Line(
+                Static("  timestamp: ", classes="exportsview--heading"),
+                Static(render_timestamp(ts)),
+            )
+
+            yield Line(Static("  symbols: ", classes="exportsview--heading"))
+
+            for entry in self.ctx.pe.DIRECTORY_ENTRY_EXPORT.symbols:
+                symbol_name = entry.name.decode("ascii")
+                yield Line(
+                    Static("    "),
+                    Static(symbol_name, classes="exportsview--symbol"),
+                )
 
 
 class NavView(Static):
@@ -1272,7 +1340,7 @@ class NavView(Static):
         self.ctx = ctx
 
     def action_navigate_to(self, id: str):
-        self.parent.query_one(f"#{id}").scroll_visible()
+        self.parent.query_one(f"#{id}").scroll_visible(top=True, animate=False)
 
     def render(self) -> str:
         lines = []
